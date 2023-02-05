20a7507f8fd05d5329c297fd4e782d4257b5b155
DenisChetyrkin <denischet01@gmail.com>
2023-02-05 16:36:17 +0300

    first commit

---
+++ projects/konstruct/telegram.php
@@ -0,0 +1,24 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$email = $_POST['user_email'];
+$token = "5720849387:AAFvMaj40yKKyQayrprZ46zsRNGBFg1wdGw";
+$chat_id = "-838827052";
+$arr = array(
+  'Email:' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
